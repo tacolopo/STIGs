1883854374aa05d42bec7d36edf059939c09373c
tacolopo <jkasi@moultrieaudits.com>
2023-12-27 12:06:27 -0500

    Create Microsoft Edge.ps1

---
+++ Windows Server/Microsoft Edge.ps1
@@ -0,0 +1,9 @@
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+"Security Technical Implementation Guide (STIG) Microsoft Edge V1R7"
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"All checks are verified with these two commands"
+"Get-ItemProperty -Path HKLM:SOFTWARE\Policies\Microsoft\Edge\"
+Get-ItemProperty -Path HKLM:SOFTWARE\Policies\Microsoft\Edge\
+"Get-ItemProperty -Path HKLM:SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallBlocklist\"
+Get-ItemProperty -Path HKLM:SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallBlocklist\
