18a1cd6a8f99033540d61eb06505a0d670cbd785
tacolopo <jkasi@moultrieaudits.com>
2023-12-27 12:08:33 -0500

    Create Defender Antivirus.ps1

---
+++ Windows Server/Defender Antivirus.ps1
@@ -0,0 +1,43 @@
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+"Security Technical Implementation Guide (STIG) Windows Defender Antivirus V2R4"
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"WNDF-AV-000001, WNDF-AV-000003, WNDF-AV-000004"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\
+
+"WNDF-AV-000005, WNDF-AV-000006, WNDF-AV-000007"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Exclusions\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Exclusions\
+
+"WNDF-AV-000008, WNDF-AV-000009, WNDF-AV-000010, WNDF-AV-000011"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Spynet\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Spynet\
+
+"WNDF-AV-000012"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\NIS\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\NIS\
+
+"WNDF-AV-000013, WNDF-AV-000014, WNDF-AV-000015, WNDF-AV-000016, WNDF-AV-000017, WNDF-AV-000018, WNDF-AV-000019, WNDF-AV-000020, WNDF-AV-000021, WNDF-AV-000022, WNDF-AV-000023"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Real-Time` Protection\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Real-Time` Protection\
+
+"WNDF-AV-000024, WNDF-AV-000025, WNDF-AV-000026, WNDF-AV-000027"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Scan\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Scan\
+
+"WNDF-AV-000028, WNDF-AV-000029, WNDF-AV-000030"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Signature` Updates\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Signature` Updates\
+
+"WNDF-AV-000031, WNDF-AV-000040, WNDF-AV-000041, WNDF-AV-000042"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Threats\ThreatSeverityDefaultAction\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Threats\ThreatSeverityDefaultAction\
+
+"WNDF-AV-000032, WNDF-AV-000033, WNDF-AV-000034, WNDF-AV-000035, WNDF-AV-000036, WNDF-AV-000037, WNDF-AV-000038"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Windows` Defender` Exploit` Guard\ASR\Rules\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Windows` Defender` Exploit` Guard\ASR\Rules\
+
+"WNDF-AV-000039"
+"Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Windows` Defender` Exploit` Guard\Network` Protection\"
+Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows` Defender\Windows` Defender` Exploit` Guard\Network` Protection\
