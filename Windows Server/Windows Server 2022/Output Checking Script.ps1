9eb0068c14595c98f6f5fcfd3837953e72cddec4
tacolopo <jkasi@moultrieaudits.com>
2024-02-22 08:39:22 -0500

    begin writing array

---
+++ Windows Server/Windows Server 2022/Output Checking Script.ps1
@@ -0,0 +1,13 @@
+$stringsToSearch = @(
+    "20348",
+    "Microsoft Defender Antivirus Service",
+    "NTFS",
+    "Network Access: Let Everyone permissions apply to anonymous users  Disabled",
+    "<AppLockerPolicy",
+    "TpmPresent                : True",
+    "SMB1                          : 0",
+    "Start        : 4",
+    "Type: NT5DS (Local)",
+    "BiosFirmwareType                                        : Uefi",
+    ""
+)
