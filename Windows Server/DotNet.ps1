21fac507f3f445160f57df74a0084754ae938e70
tacolopo <anthonyrosa@crypticrisk.com>
2024-07-18 14:03:33 -0400

    Update DotNet.ps1

--- Windows Server/DotNet.ps1
+++ Windows Server/DotNet.ps1
@@ -12,9 +12,32 @@ Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\StrongName\Verification\
 Get-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\WinTrust\Trust` Providers\Software` Publishing\
 "----------------------------------------------------------------------------------------------------------------------------------------------------------"
 
+"Primary caspol config check. If fails, defer to subsequent string searches."
+"APPNET0060, APPNET0064, APPNET0065, APPNET0066"
+"Get-Content 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\caspol.exe.config'"
+Get-Content 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\caspol.exe.config'
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"APPNET0060"
+'FINDSTR /i /s "channel" c:\*.exe.config'
+Invoke-Expression -Command 'FINDSTR /i /s "channel" c:\*.exe.config'
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
 "APPNET0061"
-"Get-WindowsFeature"
-Get-WindowsFeature
+"ls 'C:\Windows\Microsoft.NET\Framework64'"
+ls 'C:\Windows\Microsoft.NET\Framework64'
+"ls 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\'"
+ls 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\'
+"ls 'C:\Windows\Microsoft.NET\Framework'"
+ls 'C:\Windows\Microsoft.NET\Framework'
+"ls 'C:\Windows\Microsoft.NET\Framework\v1.0.3705'"
+ls 'C:\Windows\Microsoft.NET\Framework\v1.0.3705'
+"ls 'C:\Windows\Microsoft.NET\Framework\v1.1.4322'"
+ls 'C:\Windows\Microsoft.NET\Framework\v1.1.4322'
+"ls 'C:\Windows\Microsoft.NET\Framework\v2.0.50727'"
+ls 'C:\Windows\Microsoft.NET\Framework\v2.0.50727'
+"ls 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\'"
+ls 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\'
 "----------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 "APPNET0063, APPNET0075"
