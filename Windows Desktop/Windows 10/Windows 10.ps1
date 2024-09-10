7ba22eba7acce3b67ab695286ae7cd78ef28e533
tacolopo <anthonyrosa@crypticrisk.com>
2024-09-10 10:00:20 -0400

    add checks

--- Windows Desktop/Windows 10/Windows 10.ps1
+++ Windows Desktop/Windows 10/Windows 10.ps1
@@ -3,16 +3,57 @@
 "----------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 "WN10-00-000005"
-"Get-ComputerInfo Edition and Architecture"
+"Verify domain-joined systems are using Windows 10 Enterprise Edition 64-bit version"
 $os = Get-ComputerInfo
-$os.OsName
-$os.OsArchitecture
+$os.CsDomainRole #MemberWorkstation
+$os.OsName #Windows 10 Enterprise
+$os.OsArchitecture #64-bit
 "----------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 "WN10-00-000010"
-"Get-Tpm"
+"Verify TPM is present and enabled"
 $tpm = Get-Tpm
-$tpm.TpmPresent
-$tpm.TpmEnabled
+$tpm.TpmPresent #True
+$tpm.TpmEnabled #True
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"WN10-00-000015"
+"System must be configured to run in UEFI mode."
+$os.BiosFirmwareType #Uefi
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"WN10-00-000020"
+"Secure Boot must be enabled."
+$bootState = Confirm-SecureBootUEFI
+$bootState #True
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"WN10-00-000025"
+"An approved tool for continuous network scanning must be installed and configured to run."
+"True"
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"WN10-00-000030"
+"Systems must use BitLocker to encrypt all disks"
+$bitLocker = Get-BitLockerVolume
+$bitLocker.ProtectionStatus #On
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"WN10-00-000031"
+"Systems must use a BitLocker PIN for pre-boot authentication."
+$bitLockerPin = Get-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\FVE\
+$bitLockerPin.UseAdvancedStartup #1
+$bitLockerPin.UseTPMPIN #1 or 2
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"WN10-00-000032"
+"BitLocker PIN must have a minimum length of six digits for pre-boot authentication."
+$bitLockerPin.MinimumPin #6 or greater
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"WN10-00-000035"
+"Verify AppLocker is enabled"
+$appLocker = Get-AppLockerPolicy -Effective -Xml
+$appLocker.Contains('Type="Appx" EnforcementMode="Enabled"') #True
 "----------------------------------------------------------------------------------------------------------------------------------------------------------"
 
