24b8511de116a499857da51eaa22a38d789f5a29
tacolopo <anthonyrosa@crypticrisk.com>
2024-09-10 11:16:00 -0400

    add 6 checks

--- Windows Desktop/Windows 10/Windows 10.ps1
+++ Windows Desktop/Windows 10/Windows 10.ps1
@@ -2,9 +2,12 @@
 "Security Technical Implementation Guide (STIG) Microsoft Windows 10 V3R1"
 "----------------------------------------------------------------------------------------------------------------------------------------------------------"
 
+#Comment out all STIG ID Titles and descriptions. Print Out STIG ID for fail.
+
 "WN10-00-000005"
 "Verify domain-joined systems are using Windows 10 Enterprise Edition 64-bit version"
 $os = Get-ComputerInfo
+$os.CsDNSHostName
 $os.CsDomainRole #MemberWorkstation
 $os.OsName #Windows 10 Enterprise
 $os.OsArchitecture #64-bit
@@ -55,5 +58,52 @@ $bitLockerPin.MinimumPin #6 or greater
 "Verify AppLocker is enabled"
 $appLocker = Get-AppLockerPolicy -Effective -Xml
 $appLocker.Contains('Type="Appx" EnforcementMode="Enabled"') #True
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+"WN10-00-000040"
+"Get Build Number"
+$os.OsBuildNumber #19045
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "WN10-00-000045"
+# "Check Defender antivirus is running"
+$defender = get-service | where {$_.DisplayName -Like "*Defender*"} | Select Status,DisplayName | Out-String
+if ($defender.Contains('Running Microsoft Defender Antivirus') -eq $false) { Write-Output "WN10-00-000045" }
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "WN10-00-000050"
+# "Local volumes must be formatted using NTFS."
+$ntfs = Get-Volume
+$ntfsCheck = foreach ($volume in $ntfs) { if ($volume.FileSystemType -ne 'NTFS') { Write-Output "Fail" } }
+if ($ntfsCheck -eq $null ) { SilentlyContinue } else { Write-Output "WN10-00-000050" }
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "WN10-00-000055"
+# "Alternate operating systems must not be permitted on the same system."
+$alternateOS = bcdedit /enum all | Out-String
+if ($alternateOS.Contains("Windows 11") -or $alternateOS.Contains("Linux")) { Write-Output "WN10-00-000055" }
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+#Confirm whether print is allowed
+# "WN10-00-000060"
+# "Confirm only approved shares exist"
+$shares = Get-WmiObject -Class Win32_Share
+$allowedShares = @("ADMIN$", "C$", "IPC$", "print$")
+$shareNames = $shares.Name
+if ($shareNames | Where-Object { $_ -notin $allowedShares }) { Write-Output "WN10-00-000060" }
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+#Confirm whether noaccess account is allowed
+# "WN10-00-000065"
+# Confirm accounts last login
+([ADSI]('WinNT://{0}' -f $env:COMPUTERNAME)).Children | Where { $_.SchemaClassName -eq 'user' } | ForEach {
+   $user = ([ADSI]$_.Path)
+   $lastLogin = $user.Properties.LastLogin.Value
+   $enabled = ($user.Properties.UserFlags.Value -band 0x2) -ne 0x2
+   if ($lastLogin -eq $null) {
+      $lastLogin = 'Never'
+   }
+   if ($enabled -eq $true -and $user.Name -ne 'noaccess') { Write-Host "WN10-00-000065 $($user.Name) $($lastLogin) $($enabled)" }
+}
 "----------------------------------------------------------------------------------------------------------------------------------------------------------"
 
