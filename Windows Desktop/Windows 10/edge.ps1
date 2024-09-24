f98cf23be1d1b89f8b68650462fda01d27da5033
tacolopo <anthonyrosa@crypticrisk.com>
2024-09-24 07:23:09 -0400

    Add 10 checks

--- Windows Desktop/Windows 10/edge.ps1
+++ Windows Desktop/Windows 10/edge.ps1
@@ -29,3 +29,67 @@ if ($baseEdgeSettings.PreventSmartScreenPromptOverrideForFiles -ne 1) { Write-Ou
 # "This requirement for 'SmartScreenAllowListDomains' is not required; this is optional."
 
 "----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000005"
+# "InPrivate mode must be disabled."
+if ($baseEdgeSettings.InPrivateModeAvailability -ne 1) { Write-Output "EDGE-00-000005" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000006"
+# "Background processing must be disabled."
+if ($baseEdgeSettings.BackgroundModeEnabled -ne 0) { Write-Output "EDGE-00-000006" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000008"
+# "The ability of sites to show pop-ups must be disabled."
+if ($baseEdgeSettings.DefaultPopupsSetting -ne 2) { Write-Output "EDGE-00-000008" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000009"
+# "The default search provider must be set to use an encrypted connection."
+$edgeManagedSearchEngines = $baseEdgeSettings.ManagedSearchEngines | ConvertFrom-Json
+$edgeNonHttpsSearchEngines = $edgeManagedSearchEngines | Where-Object { $_.search_url -notmatch '^https://' }
+
+if ($edgeNonHttpsSearchEngines) { Write-Output "EDGE-00-000009" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000010"
+# "Data Synchronization must be disabled."
+if ($baseEdgeSettings.SyncDisabled -ne 1) { Write-Output "EDGE-00-000010" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000011"
+# "Network prediction must be disabled."
+if ($baseEdgeSettings.NetworkPredictionOptions -ne 2) { Write-Output "EDGE-00-000011" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000012"
+# "Search suggestions must be disabled."
+if ($baseEdgeSettings.SearchSuggestEnabled -ne 0) { Write-Output "EDGE-00-000012" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000013"
+# "Importing of autofill form data must be disabled."
+if ($baseEdgeSettings.ImportAutofillFormData -ne 0) { Write-Output "EDGE-00-000013" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000014"
+# "Importing of browser settings must be disabled."
+if ($baseEdgeSettings.ImportBrowserSettings -ne 0) { Write-Output "EDGE-00-000014" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+# "EDGE-00-000015"
+# "Importing of cookies must be disabled."
+if ($baseEdgeSettings.ImportCookies -ne 0) { Write-Output "EDGE-00-000015" }
+
+"----------------------------------------------------------------------------------------------------------------------------------------------------------"
+
